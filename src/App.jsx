0bafaed881b0c0b399501631eb8a171480d4c4c3
rubenaguilardev <ragui56@wgu.edu>
2025-07-16 03:21:18 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -76,11 +76,11 @@ const App = () => {
       style={{  zIndex: 0, width: '100%', pointerEvents: 'none', overflow: 'hidden' }}/>}
       <div className='w-full overflow-x-hidden'>
         <main className='container zIndex-10 mx-auto flex flex-col items-center px-3 pb-3 text-center'>
-          <section className='flex flex-col items-center mt-40 h-[100vh] px'>
+          <section className='flex flex-col items-center justify-center h-[100vh] px'>
             <h1 className='text-[2.8rem] mb-5'>Today is <span className="text-[#ffbcff]"> Coope's </span> Birthday! 🎂
             </h1>
             <img 
-              className='w-[260px] border-5 border-[rgb(84,33,126)] rounded-[50%] mb-8 shadow-2xl' 
+              className='w-[240px] border-5 border-[rgb(84,33,126)] rounded-[50%] mb-8 shadow-2xl' 
               src={coope} 
               alt="Picture of Cooper"
             />
