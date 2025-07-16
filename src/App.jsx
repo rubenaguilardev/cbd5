3f8603e41f0a3e30af559cbc38859f6b14467b96
rubenaguilardev <ragui56@wgu.edu>
2025-07-16 02:46:21 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -84,7 +84,7 @@ const App = () => {
           <h1 className='text-5xl mb-5'>Today is <span className="text-[#ffbcff]"> Coope's </span> Birthday! 🎂
           </h1>
           <img 
-            className='w-[280px] border-5 border-[rgb(84,33,126)] rounded-[50%] mb-8 shadow-2xl' 
+            className='w-[260px] border-5 border-[rgb(84,33,126)] rounded-[50%] mb-8 shadow-2xl' 
             src={coope} 
             alt="Picture of Cooper"
           />
